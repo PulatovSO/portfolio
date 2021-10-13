934a2a317687b4109eb197945099142044a4684f
SO Pulatov <SirotilloPulatov@gmail.com>
2021-10-13 08:10:16 +0500

    home page

--- js/main.js
+++ js/main.js
@@ -3,17 +3,24 @@ let menu = document.querySelector('home__menu');
 let menuLeft = document.querySelector('.navigation__left');
 let menuRight = document.querySelector('.navigation__right');
 
-// document.querySelector('bg-video').defaultPlaybackRate = 0.5;
-// document.querySelector('video').play();
-
 document.querySelector('video').playbackRate = 0.7;
-
 
 function toggleMenu() {
   menuLeft.classList.toggle('menu-left__show');
   menuRight.classList.toggle('menu-right__show');
 }
 
+let cursor = document.querySelector('.cursor');
+document.addEventListener('mousemove', (e) => {
+  cursor.style.left = e.pageX + 'px';
+  cursor.style.top = e.pageY + 'px';
+})
 
+document.addEventListener('click', () => {
+  cursor.classList.add('expand')
 
+  setTimeout(() => {
+    cursor.classList.remove('expand')
+  }, 500)
+})
 
