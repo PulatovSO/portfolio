617e26131e4ed1080ceb229969d55db4077f2383
SO Pulatov <SirotilloPulatov@gmail.com>
2021-10-14 18:09:56 +0500

    added contacts section

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 let aboutBtn = document.querySelector('.about-hidden');
 let portfolioBtn = document.querySelector('.works-hidden')
+let contactsBtn = document.querySelector('.contacts-hidden')
 
 function menuClick() {
   let navBar = document.querySelector('.menu');
@@ -9,31 +10,37 @@ function menuClick() {
 function aboutDrag() {
   aboutBtn.classList.toggle('show-page')
   portfolioBtn.classList.remove('show-page')
+  contactsBtn.classList.remove('show-page')
 }
 function portfolioDrag() {
   portfolioBtn.classList.toggle('show-page')
   aboutBtn.classList.remove('show-page')
-
+  contactsBtn.classList.remove('show-page')
+}
+function contactsDrag() {
+  aboutBtn.classList.remove('show-page')
+  portfolioBtn.classList.remove('show-page')
+  contactsBtn.classList.toggle('show-page')
 }
 
 function homeBtn () {
   aboutBtn.classList.remove('show-page')
   portfolioBtn.classList.remove('show-page')
+  contactsBtn.classList.remove('show-page')
 }
 
 
-// cursor
-let cursor = document.querySelector('.cursor');
-document.addEventListener('mousemove', (e) => {
-  cursor.style.left = e.pageX + 'px';
-  cursor.style.top = e.pageY + 'px';
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: "+(e.pageY - 0)+"px; left: "+(e.pageX - 0)+"px;")
 })
 
 document.addEventListener('click', () => {
-  cursor.classList.add('expand')
+    cursor.classList.add("expand");
 
-  setTimeout(() => {
-    cursor.classList.remove('expand')
-  }, 500)
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500)
 })
 
